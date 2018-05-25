853410b8f6f7a657ab0a8e9ca21d04ed4cfd7797
SlightlyObscure <33038788+SlightlyObscure@users.noreply.github.com>
2018-05-25 15:36:03 +0200

    options & stuff

--- myfind.cpp
+++ myfind.cpp
@@ -9,7 +9,53 @@ void print_usage() {
 
 
 int main(int argc, char* argv[]) {
-	cout << "Hello World?" << endl;
-	print_usage();
+	int c;
+	int optc=0;
+	int error=0;	
+	int option_R=0;
+	int option_i=0;
+
+	while((c = getopt(argc, argv, "Ri:")) != EOF) {
+		switch(c) {
+			case 'R':
+				if(option_R) {
+					error=1;
+					break;
+				}
+				option_R=1;
+				optc++;
+				cout << "myfind: parsing option R" << endl;
+				break;
+			case 'i':
+				if (option_i) {
+					error=1;
+					break;
+				}
+				option_i=1;
+				optc++;
+				cout << "myfind: parsing option i" << endl;
+				break;
+			case '?':
+				error=1;
+			default:
+				return 1;
+			
+		}
+	}
+	if (error || argc-optc<3) {
+		print_usage();
+		return 1;
+		
+	}
+	
+	for(int i=0; i<argc; i++) {
+		cout << "myfind: parsing argument " << argv[i] << endl;
+	}
 	return 0;
+
 }
+
+
+
+
+
