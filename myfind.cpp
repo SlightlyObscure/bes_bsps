394140ce48ecfca4cc531cde71086d1169a6d3d6
SlightlyObscure <33038788+SlightlyObscure@users.noreply.github.com>
2018-05-25 18:45:16 +0200

    alles bis auf ganzen searchPath angeben und forks für jedes gesuchte wort

--- myfind.cpp
+++ myfind.cpp
@@ -2,12 +2,58 @@
 #include <getopt.h>
 #include <string>
 #include <dirent.h>
-
+#include <string.h>
 
 using namespace std;
 
 void print_usage() {
 	cerr << "Correct usage: \"myfind [-R] [-i] searchpath filename1 [filename2]...[filename]\"" << endl;
+}
+
+int file_searcher(int argc, char* argv[], int optc, int option_i, int option_R, const char* searchPath) {
+	//experimental
+
+	
+	struct dirent *direntp;
+	DIR *dirp;
+	if((dirp = opendir(searchPath)) == NULL) {
+		cerr << "Error! Failed to open directory or file is not a directory" << endl;
+		return 1;
+			
+	}
+	while((direntp = readdir(dirp)) != NULL) {
+		if(direntp->d_name[0] != '.') {
+			cout << direntp->d_name << endl;
+			for(int i=2+optc; i<argc; i++) {
+		                if(option_i) {
+					if(strcasecmp(direntp->d_name, argv[i]) == 0) {
+						cout << "myfind: found " << argv[i] << " at " << searchPath  << endl;
+					}
+				}
+				else {
+					if(strcmp(direntp->d_name, argv[i]) == 0) {
+                                                cout << "myfind: found " << argv[i] << " at " << searchPath  << endl;
+                                        }
+				}
+       			}
+			if(option_R) {
+				string PathPart1(searchPath);
+				string PathPart2(direntp->d_name);
+				string almost_next = PathPart1 + PathPart2 + "/";
+				const char* searchPath_next = almost_next.c_str();
+				cout << "recursive search at: " << searchPath_next << endl;
+				file_searcher(argc, argv, optc, option_i, option_R, searchPath_next);
+			}
+		}
+	}
+	while ((closedir(dirp) == -1) && (errno == EINTR));
+	
+
+	return 0;
+
+	
+	//end experimental
+	
 }
 
 
@@ -17,7 +63,7 @@ int main(int argc, char* argv[]) {
 	int error=0;	
 	int option_R=0;
 	int option_i=0;
-	char* searchPath;
+	const char* searchPath;
 	
 	
 
@@ -63,34 +109,22 @@ int main(int argc, char* argv[]) {
 	searchPath = argv[1+optc];
 	cout << "myfind: searchpath is " << searchPath <<endl;	
 	
+	DIR *dirp;
+	if((dirp = opendir(argv[1+optc])) == NULL) {
+                cerr << "Error! Failed to open directory" << endl;
+                print_usage();
+                return 1;
+        }
+	while ((closedir(dirp) == -1) && (errno == EINTR));	
 
 	for(int i=2+optc; i<argc; i++) {
-                cout << "myfind: searching for " << argv[i] << endl;
+        	cout << "myfind: searching for " << argv[i] << endl;
         }
 
 
-	//experimental
-	
-	struct dirent *direntp;
-	DIR *dirp;
-	if((dirp = opendir(argv[1+optc])) == NULL) {
-		cerr << "Error! Failed to open directory" << endl;
-		print_usage();
-		return 1;	
-	}
-	while((direntp = readdir(dirp)) != NULL) {
-		if(direntp->d_name[0] != '.') {
-			cout << direntp->d_name << endl;
-			for(int i=2+optc; i<argc; i++) {
-		                if(*direntp->d_name == *argv[i]) {
-					cout << "myfind: found " << argv[i] << " at " << searchPath  << endl;
-				}
-       			 }
-		}
-	}
-	while ((closedir(dirp) == -1) && (errno == EINTR));
-		
-	//end experimental
+	file_searcher(argc, argv, optc, option_i, option_R, searchPath);	
+
+	return 0;	
 
 }
 
